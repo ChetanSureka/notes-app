222737779aa5f769f998545d52519b3504a29282
ChetanSureka <surekachetan09@gmail.com>
2023-05-03 20:55:11 +0530

    integrated the react app with django backend

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # BASE_DIR / 'frontend/build'
+            BASE_DIR / 'frontend/r_build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    # BASE_DIR / 'frontend/build/static'
+    BASE_DIR / 'frontend/r_build/static'
 ]
 
 # Default primary key field type
--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
